920c0ab701eff0859e3fc6b70ff4ba61f1dc1e34
Animesh Paul <animeshpaul.connect@gmail.com>
2019-08-28 12:48:28 +0530

    fix conflict

--- src/Containers/Building/index.jsx
+++ src/Containers/Building/index.jsx
@@ -1,137 +1,118 @@
-import React, { Component, Fragment } from "react";
+import React, { Component, Fragment } from 'react';
 
-import Cover from "../../Components/Cover";
-import Rooms from "../../Components/Rooms";
-import Gallery from "../../Components/Gallery";
-import Inclusive from "../../Components/Inclusive";
-import GoogleStaticMap from "../../Components/GoogleStaticMap";
-import OtherProperties from "../../Components/OtherProperties";
-import Footer from "../../Components/Footer";
-import Header from "../../Components/Header";
+import Cover from '../../Components/Cover';
+import Rooms from '../../Components/Rooms';
+import Gallery from '../../Components/Gallery';
+import Inclusive from '../../Components/Inclusive';
+import GoogleStaticMap from '../../Components/GoogleStaticMap';
+import OtherProperties from '../../Components/OtherProperties';
+import Footer from '../../Components/Footer';
+import Header from '../../Components/Header';
 
-import coverImg from "../../static/images/chill.jpg";
-import LandingCover from "./LandingCover";
-import Yellow2nut from "../../Components/Yellow2nut";
+import coverImg from '../../static/images/chill.jpg';
+import LandingCover from './LandingCover';
+import Yellow2nut from '../../Components/Yellow2nut';
 
 // css starts
-import "./style.css";
+import './style.css';
 
 class Building extends Component {
-  state = {
-    buildingData: {},
-    nearbyProperties: {},
-    footer: {
-      logoLink: "https://grexter.in",
-      phone: "8880968000",
-      instagram: "https://www.instagram.com/grexter_living/",
-      facebook: "https://www.facebook.com/grexterhousing/",
-      linkedin: "https://www.linkedin.com/company/grexter"
-    },
-    bookVisitClicked: false,
-    selectOptionsar: []
-  };
+	state = {
+		buildingData: {},
+		nearbyProperties: {},
+		footer: {
+			logoLink: 'https://grexter.in',
+			phone: '8880968000',
+			instagram: 'https://www.instagram.com/grexter_living/',
+			facebook: 'https://www.facebook.com/grexterhousing/',
+			linkedin: 'https://www.linkedin.com/company/grexter'
+		},
+		bookVisitClicked: false
+	};
 
-  async componentWillMount() {
-    let params = window.location.search;
-    // let params = queryString.parse(url);
-    const id = params.split("=")[1];
-    console.log(id);
-    let f = 35;
-    const res = await fetch(
-      `https://backend.grexter.in/buildings/${id}?include=location,amenities,landmarks,area,subarea`
-    );
-    const buildingData = await res.json();
+	async componentDidMount() {
+		let params = window.location.search;
+		// let params = queryString.parse(url);
+		const id = params.split('=')[1];
+		console.log('id is ===', id);
 
-    const response = await fetch(
-      `https://backend.grexter.in/nearby-buildings?include=location,landmarks,area,layouts,layout_prices,building_images&lon=&lat=`
-    );
-    const allBuildingsData = await response.json();
-    console.log(allBuildingsData);
+		const res = await fetch(
+			`https://backend.grexter.in/buildings/${id}?include=location,amenities,landmarks,area,subarea`
+		);
+		const buildingData = await res.json();
 
-    const selectOptionsar = [];
+		// const response = await fetch(
+		//   `https://backend.grexter.in/buildings/${id}?include=location,amenities,landmarks,area,subarea`
+		// );
+		// const allBuildingsData = await response.json();
 
-    allBuildingsData.map(data => {
-      selectOptionsar.push({
-        name: data.name,
-        id: data.id
-      });
-    });
+		// const aa= [];
 
-    const res1 = await fetch(
-      `https://backend.grexter.in/nearby-buildings?include=location,landmarks,area,layouts,layout_prices,building_images&lon=${buildingData.location.longitude}&lat=${buildingData.location.latitude}`
-    );
+		// for (const key in allBuildingsData) {
 
-    let nearbyProperties = await res1.json();
-    nearbyProperties = nearbyProperties.slice(1, 4);
-    this.setState({ buildingData, nearbyProperties, selectOptionsar });
-  }
+		//   }
+		// }
 
-  bookVisitClickHandler = () => {
-    window.scrollTo({ top: 0, behavior: "smooth" });
-    setTimeout(() => {
-      this.setState({
-        bookVisitClicked: true
-      });
-    }, 800);
-  };
-  render() {
-    const { nearbyProperties, bookVisitClicked, selectOptionsar } = this.state;
-    const {
-      address,
-      images,
-      name,
-      id,
-      location,
-      description,
-      layouts = []
-    } = this.state.buildingData;
-    const { logoLink } = this.state.footer;
+		const res1 = await fetch(
+			`https://backend.grexter.in/nearby-buildings?include=location,landmarks,area,layouts,layout_prices,building_images&lon=${buildingData
+				.location.longitude}&lat=${buildingData.location.latitude}`
+		);
 
-    // const { subLayouts } = layouts.subLayouts;
+		let nearbyProperties = await res1.json();
+		nearbyProperties = nearbyProperties.slice(1, 4);
+		this.setState({ buildingData, nearbyProperties });
+	}
 
-    console.log(
-      "address====",
-      address,
-      "images====",
-      images,
-      "name====",
-      name,
-      "id====",
-      id,
-      "location====",
-      location,
-      "description====",
-      description,
-      "layouts====",
-      layouts
-    );
+	bookVisitClickHandler = () => {
+		window.scrollTo({ top: 0, behavior: 'smooth' });
+		setTimeout(() => {
+			this.setState({
+				bookVisitClicked: true
+			});
+		}, 800);
+	};
+	render() {
+		const { nearbyProperties, bookVisitClicked } = this.state;
+		const { address, images, name, id, location, description, layouts = [] } = this.state.buildingData;
+		const { logoLink } = this.state.footer;
 
-    return (
-      <Fragment>
-        <Header />
-        <Cover images={images}>
-          <LandingCover
-            name={name}
-            desc={description}
-            bookVisitClicked={bookVisitClicked}
-            selectOptionsar={selectOptionsar}
-          />
-        </Cover>
-        <Yellow2nut text="Rooms" />
-        <Rooms
-          layouts={layouts}
-          bookVisitClickHandler={this.bookVisitClickHandler}
-        />
-        <Yellow2nut text="Inclusive" />
-        <Inclusive />
-        <Yellow2nut text="Address and Maps  " />
-        <GoogleStaticMap address={address} location={location} />
-        <Yellow2nut text="Other Properties" />
-        <OtherProperties nearby={nearbyProperties} />
-        <Footer {...this.state.footer} />
-      </Fragment>
-    );
-  }
+		// const { subLayouts } = layouts.subLayouts;
+
+		console.log(
+			'address====',
+			address,
+			'images====',
+			images,
+			'name====',
+			name,
+			'id====',
+			id,
+			'location====',
+			location,
+			'description====',
+			description,
+			'layouts====',
+			layouts
+		);
+
+		return (
+			<Fragment>
+				<Header />
+				<Cover images={images}>
+					<LandingCover name={name} desc={description} bookVisitClicked={bookVisitClicked} />
+				</Cover>
+				<Yellow2nut text="Rooms" />
+				<Rooms layouts={layouts} bookVisitClickHandler={this.bookVisitClickHandler} />
+				<Yellow2nut text="Inclusive" />
+				<Inclusive />
+				<Yellow2nut text="Address and Maps  " />
+				<GoogleStaticMap address={address} location={location} />
+				<Yellow2nut text="Other Properties" />
+				<OtherProperties nearby={nearbyProperties} />
+				<Footer {...this.state.footer} />
+			</Fragment>
+		);
+	}
 }
 
 export default Building;
--- src/components/Form/index.jsx
+++ src/components/Form/index.jsx
@@ -1,189 +1,180 @@
 //node modules
-import React, { Component } from "react";
-import { Input, FormGroup, Label, FormFeedback } from "reactstrap";
-import SuccessModal from "../SuccessModal";
+import React, { Component } from 'react';
+import { Input, FormGroup, Label, FormFeedback } from 'reactstrap';
+import SuccessModal from '../SuccessModal';
 //components
 
 //styling
-import "./style.css";
+import './style.css';
 
 class ScheduleVisit extends Component {
-  state = {
-    contact_number: "",
-    // layout_id: 6,
-    // message: "",
-    name: "",
-    selectedInterestOption: "Hi",
-    // interestOptions : [],
-    // status: "",
-    toast: false,
-    loader: false,
-    err: false,
-    validate: {}
-  };
+	state = {
+		contact_number: '',
+		// layout_id: 6,
+		// message: "",
+		name: '',
+		dropdown: '1',
+		selectedInterestOption: 'Hi',
+		// interestOptions : [],
+		// status: "",
+		toast: false,
+		loader: false,
+		err: false,
+		validate: {}
+	};
 
-  handleChange = evt => {
-    this.setState({ [evt.target.name]: evt.target.value, err: false });
-  };
+	handleChange = (evt) => {
+		this.setState({ [evt.target.name]: evt.target.value, err: false });
+	};
 
-  handleCheck = event => {
-    event.preventDefault();
-    const { name, contact_number } = this.state;
+	handleCheck = (event) => {
+		event.preventDefault();
+		const { name, contact_number } = this.state;
 
-    const { validate } = this.state;
-    if (name === "") {
-      validate.nameErr = true;
-    }
-    if (contact_number === "") {
-      validate.phnumErr = true;
-    }
+		const { validate } = this.state;
+		if (name === '') {
+			validate.nameErr = true;
+		}
+		if (contact_number === '') {
+			validate.phnumErr = true;
+		}
 
-    this.setState(validate);
+		this.setState(validate);
 
-    if (validate.nameErr || validate.phnumErr) {
-      this.setState({ err: true });
-    } else {
-      this.setState({ err: false }, event => {
-        console.log(event);
-        this.handleSubmit(event);
-      });
-    }
-  };
+		if (validate.nameErr || validate.phnumErr) {
+			this.setState({ err: true });
+		} else {
+			this.setState({ err: false }, (event) => {
+				console.log(event);
+				this.handleSubmit(event);
+			});
+		}
+	};
 
-  handleNameChange = async evt => {
-    await this.setState({ [evt.target.name]: evt.target.value, err: false });
+	handleNameChange = async (evt) => {
+		await this.setState({ [evt.target.name]: evt.target.value, err: false });
 
-    const { name } = this.state;
+		const { name } = this.state;
 
-    const { validate } = this.state;
+		const { validate } = this.state;
 
-    if (/^[A-z ]+$/.test(name)) {
-      validate.nameErr = false;
-    } else {
-      validate.nameErr = true;
-    }
+		if (/^[A-z ]+$/.test(name)) {
+			validate.nameErr = false;
+		} else {
+			validate.nameErr = true;
+		}
 
-    this.setState({ validate });
-    console.log(validate);
-  };
+		this.setState({ validate });
+		console.log(validate);
+	};
 
-  handelPhnumChange = async evt => {
-    await this.setState({ [evt.target.name]: evt.target.value, err: false });
+	handelPhnumChange = async (evt) => {
+		await this.setState({ [evt.target.name]: evt.target.value, err: false });
 
-    const { contact_number } = this.state;
+		const { contact_number } = this.state;
 
-    const { validate } = this.state;
+		const { validate } = this.state;
 
-    if (/^\d{10}$/.test(contact_number)) {
-      validate.phnumErr = false;
-    } else {
-      validate.phnumErr = true;
-    }
-    this.setState({ validate });
-    console.log(validate);
-  };
+		if (/^\d{10}$/.test(contact_number)) {
+			validate.phnumErr = false;
+		} else {
+			validate.phnumErr = true;
+		}
+		this.setState({ validate });
+		console.log(validate);
+	};
+	handelSelectChange = async (evt) => {
+		await this.setState({ [evt.target.name]: evt.target.value, err: false });
+		console.log(this.state);
+	};
+	handleSubmit = (e) => {
+		this.setState({ loader: true, toast: true });
+	};
 
-  handleSubmit = e => {
-    this.setState({ loader: true, toast: true });
-  };
-
-  render() {
-    // prettier-ignore
-    const { toast, loader, err, name, contact_number,  validate ,selectedInterestOption} = this.state;
-    const { bookVisitClicked, selectOptionsar } = this.props;
-    if (bookVisitClicked === true) {
-      document.getElementById("name").focus();
-    }
-    return (
-      <div className="form">
-        <p className="formHeader">Let we Know You..!</p>
-        <form>
-          <FormGroup>
-            <label style={{ fontSize: "14px", fontWeight: "bold" }}>
-              Name :
-            </label>
-            <Input
-              id="name"
-              type="text"
-              placeholder="Name"
-              className="input-text"
-              invalid={false}
-              name="name"
-              value={name}
-              onChange={this.handleNameChange}
-              invalid={validate.nameErr}
-            />
-            <FormFeedback invalid>Please enter a valid name</FormFeedback>
-          </FormGroup>
-          <FormGroup>
-            <label style={{ fontSize: "14px", fontWeight: "bold" }}>
-              Phone :
-            </label>
-            <Input
-              maxLength="10"
-              type="text"
-              placeholder="Phone Number"
-              className="input-text"
-              invalid={false}
-              name="contact_number"
-              value={contact_number}
-              onChange={this.handelPhnumChange}
-              invalid={validate.phnumErr}
-            />
-            <FormFeedback invalid>
-              Please enter a valid phone number
-            </FormFeedback>
-          </FormGroup>
-          <FormGroup>
-            <label style={{ fontSize: "14px", fontWeight: "bold" }}>
-              Your interest :
-            </label>
-            <Input
-              maxLength="10"
-              type="select"
-              placeholder="Phone Number"
-              className="input-text"
-              invalid={false}
-              value={selectedInterestOption}
-            >
-              {}
-            </Input>
-            <FormFeedback invalid>
-              Please enter a valid phone number
-            </FormFeedback>
-          </FormGroup>
-          <FormGroup>
-            <button
-              type="submit"
-              className="btn bookAVisit submit"
-              onClick={this.handleCheck}
-            >
-              Submit
-            </button>
-          </FormGroup>
-        </form>
-        {err ? (
-          <div className="error-text">
-            Please Fill in all the fields before proceeding
-          </div>
-        ) : null}
-        <div>{loader ? <div className="loader" /> : null}</div>
-        {toast ? (
-          <SuccessModal
-            isOpen={toast}
-            closeModal={() => {
-              this.setState({ toast: false });
-            }}
-          >
-            <div className="sched-suc-modal-h">Great!</div>
-            <div className="sched-suc-modal-b">
-              We will contact you within 24 to 48 hours..!
-            </div>
-          </SuccessModal>
-        ) : null}
-      </div>
-    );
-  }
+	render() {
+		// prettier-ignore
+		const { toast, loader, err, name, contact_number,  validate ,selectedInterestOption} = this.state;
+		const { bookVisitClicked } = this.props;
+		if (bookVisitClicked === true) {
+			document.getElementById('name').focus();
+		}
+		return (
+			<div className="form">
+				<p className="formHeader">Let we Know You..!</p>
+				<form>
+					<FormGroup>
+						<label style={{ fontSize: '14px', fontWeight: 'bold' }}>Name :</label>
+						<Input
+							id="name"
+							autoFocus
+							type="text"
+							placeholder="Name"
+							className="input-text"
+							invalid={false}
+							name="name"
+							value={name}
+							onChange={this.handleNameChange}
+							invalid={validate.nameErr}
+						/>
+						<FormFeedback invalid>Please enter a valid name</FormFeedback>
+					</FormGroup>
+					<FormGroup>
+						<label style={{ fontSize: '14px', fontWeight: 'bold' }}>Phone :</label>
+						<Input
+							maxLength="10"
+							type="text"
+							placeholder="Phone Number"
+							className="input-text"
+							invalid={false}
+							name="contact_number"
+							value={contact_number}
+							onChange={this.handelPhnumChange}
+							invalid={validate.phnumErr}
+						/>
+						<FormFeedback invalid>Please enter a valid phone number</FormFeedback>
+					</FormGroup>
+					<FormGroup>
+						<label style={{ fontSize: '14px', fontWeight: 'bold' }}>Your interest :</label>
+						<Input
+							maxLength="10"
+							type="select"
+							placeholder="Phone Number"
+							className="input-text"
+							invalid={false}
+							name="dropdown"
+							value={selectedInterestOption}
+							onChange={this.handelSelectChange}
+							invalid={validate.phnumErr}
+						>
+							<option>1</option>
+							<option>2</option>
+							<option>3</option>
+							<option>4</option>
+							<option>5</option>
+						</Input>
+					</FormGroup>
+					<FormGroup>
+						<button type="submit" className="btn bookAVisit submit" onClick={this.handleCheck}>
+							Submit
+						</button>
+					</FormGroup>
+				</form>
+				{err ? <div className="error-text">Please Fill in all the fields before proceeding</div> : null}
+				<div>{loader ? <div className="loader" /> : null}</div>
+				{toast ? (
+					<SuccessModal
+						isOpen={toast}
+						closeModal={() => {
+							this.setState({ toast: false });
+						}}
+					>
+						<div className="sched-suc-modal-h">Great!</div>
+						<div className="sched-suc-modal-b">We will contact you within 24 to 48 hours..!</div>
+					</SuccessModal>
+				) : null}
+			</div>
+		);
+	}
 }
 
 export default ScheduleVisit;
