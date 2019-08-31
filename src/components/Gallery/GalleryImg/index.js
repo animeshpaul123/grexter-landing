46482bd5ca9ce9dc372aa878001f8115cb1b8e55
Animesh Paul <animeshpaul.connect@gmail.com>
2019-08-31 16:47:59 +0530

    added modal gallery

--- src/components/Gallery/GalleryImg/index.js
+++ src/components/Gallery/GalleryImg/index.js
@@ -3,14 +3,14 @@ import { Col } from 'reactstrap';
 import './index.css';
 
 const GalleryImg = (props) => {
-	const { src, altName, sm, md } = props;
+	const { src, altName, sm, md, showModal } = props;
 	let style = {
 		padding: md >= 6 ? 8 : 6
 	};
 	return (
 		<Col sm={sm} md={md}>
 			<div className="gallery-img" style={style}>
-				<img src={src} alt={altName ? altName : 'gallery'} />
+				<img src={src} alt={altName ? altName : 'gallery'} onClick={showModal} />
 			</div>
 		</Col>
 	);
