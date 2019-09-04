7b1395d1bf2027ca34b5983e93b027f952683778
Animesh Paul <animeshpaul.connect@gmail.com>
2019-09-04 19:01:16 +0530

    Updates

--- precache-manifest.6d6cbb51546f2044d7d8d134ab7876a6.js
+++ precache-manifest.6d6cbb51546f2044d7d8d134ab7876a6.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "3a9659f8525c09d1160625657a69405f",
+    "revision": "2eee10f8898b3e37d3e81f929a001e90",
     "url": "/grexter-landing/index.html"
   },
   {
@@ -8,15 +8,15 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/grexter-landing/static/css/2.7eb17109.chunk.css"
   },
   {
-    "revision": "5a79de3b7b34f3cfcb42",
-    "url": "/grexter-landing/static/css/main.df98c24e.chunk.css"
+    "revision": "a291d11325f0eabfe44e",
+    "url": "/grexter-landing/static/css/main.05b8b26e.chunk.css"
   },
   {
     "revision": "293097a221ac65ce1d8e",
     "url": "/grexter-landing/static/js/2.6507ba5a.chunk.js"
   },
   {
-    "revision": "5a79de3b7b34f3cfcb42",
+    "revision": "a291d11325f0eabfe44e",
     "url": "/grexter-landing/static/js/main.04dc04e0.chunk.js"
   },
   {
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/grexter-landing/precache-manifest.00dd7b3928d1c826a8f8bcf19321d0b5.js"
+  "/grexter-landing/precache-manifest.6d6cbb51546f2044d7d8d134ab7876a6.js"
 );
 
 self.addEventListener('message', (event) => {
