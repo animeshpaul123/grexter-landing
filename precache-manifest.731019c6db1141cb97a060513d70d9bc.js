092e5289a67ac9db75f796db4de07b28b6fa9ca4
Animesh Paul <animeshpaul.connect@gmail.com>
2019-09-04 12:58:35 +0530

    Updates

---
+++ precache-manifest.731019c6db1141cb97a060513d70d9bc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "809f2da1bee46807d0615cffdc28a6cb",
+    "url": "/grexter-landing/index.html"
+  },
+  {
+    "revision": "54f9593b17b53a500c40",
+    "url": "/grexter-landing/static/css/2.7eb17109.chunk.css"
+  },
+  {
+    "revision": "83ba9fde8ad80e6d4e49",
+    "url": "/grexter-landing/static/css/main.ee697a6d.chunk.css"
+  },
+  {
+    "revision": "54f9593b17b53a500c40",
+    "url": "/grexter-landing/static/js/2.27a71d4e.chunk.js"
+  },
+  {
+    "revision": "83ba9fde8ad80e6d4e49",
+    "url": "/grexter-landing/static/js/main.b52efbb4.chunk.js"
+  },
+  {
+    "revision": "3d310e41c68c8b3485b6",
+    "url": "/grexter-landing/static/js/runtime~main.935d7ac3.js"
+  }
+]);
