1d394984d401004d81bcd623578e64aeef3787bc
Animesh Paul <animeshpaul.connect@gmail.com>
2019-09-09 15:18:23 +0530

    amenities changed to carousel in mobile view

--- src/Components/Inclusive/index.jsx
+++ src/Components/Inclusive/index.jsx
@@ -1,62 +1,121 @@
-import React from "react";
-import Backup from "../../static/images/backup.png";
-import Bills from "../../static/images/bills.png";
-import Cafe from "../../static/images/cafe.png";
-import Cleaning from "../../static/images/cleaning.png";
-import Furniture from "../../static/images/furniture.png";
-import Gaming from "../../static/images/gaming.png";
-import Security from "../../static/images/security.png";
-import Internet from "../../static/images/internet.png";
+import React, { Component } from 'react';
+import Backup from '../../static/images/backup.png';
+import Bills from '../../static/images/bills.png';
+import Cafe from '../../static/images/cafe.png';
+import Cleaning from '../../static/images/cleaning.png';
+import Furniture from '../../static/images/furniture.png';
+import Gaming from '../../static/images/gaming.png';
+import Security from '../../static/images/security.png';
+import Internet from '../../static/images/internet.png';
+import Slider from 'react-slick';
+import { FaArrowRight, FaArrowLeft } from 'react-icons/fa';
 
-import "./style.css";
+import './style.css';
 
-const Inclusives = () => {
-  const items = [
-    {
-      name: "24x7 Lift & Power Backup",
-      img: Backup
-    },
-    {
-      name: "All Bills Coverd",
-      img: Bills
-    },
-    {
-      name: "Cafetaria",
-      img: Cafe
-    },
-    {
-      name: "Regular House Cleaning",
-      img: Cleaning
-    },
-    {
-      name: "Fully Furnished",
-      img: Furniture
-    },
-    {
-      name: "Dedicated Gaming Zone",
-      img: Gaming
-    },
-    {
-      name: "Round-the-clock Security",
-      img: Security
-    },
-    {
-      name: "High Speed Internet",
-      img: Internet
-    }
-  ];
-  return (
-    <div className="icons-wrapper d-flex justify-content-around">
-      {items.map(item => {
-        return (
-          <div className="icon" key={item.name}>
-            <img src={item.img} alt={item.img} className="img lazy" />
-            <p className="text">{item.name}</p>
-          </div>
-        );
-      })}
-    </div>
-  );
-};
+class Inclusives extends Component {
+	items = [
+		{
+			name: '24x7 Lift & Power Backup',
+			img: Backup
+		},
+		{
+			name: 'All Bills Coverd',
+			img: Bills
+		},
+		{
+			name: 'Cafetaria',
+			img: Cafe
+		},
+		{
+			name: 'Regular House Cleaning',
+			img: Cleaning
+		},
+		{
+			name: 'Fully Furnished',
+			img: Furniture
+		},
+		{
+			name: 'Dedicated Gaming Zone',
+			img: Gaming
+		},
+		{
+			name: 'Round-the-clock Security',
+			img: Security
+		},
+		{
+			name: 'High Speed Internet',
+			img: Internet
+		}
+	];
+	NextArrow = (props) => {
+		const { onClick } = props;
+		return (
+			<div className="f-arrow-btn next-arrow-btn" onClick={onClick}>
+				<FaArrowRight className="f-arrow-svg" />
+			</div>
+		);
+	};
+	PrevArrow = (props) => {
+		const { onClick } = props;
+		return (
+			<div className="f-arrow-btn prev-arrow-btn" onClick={onClick}>
+				<FaArrowLeft className="f-arrow-svg" />
+			</div>
+		);
+	};
+	render() {
+		const settings = {
+			className: 'center',
+			centerMode: false,
+			infinite: true,
+			dots: false,
+			lazyLoad: true,
+			autoplay: false,
+			autoplaySpeed: 2000,
+			arrows: true,
+			speed: 500,
+			slidesToShow: 1,
+			slidesToScroll: 1,
+			nextArrow: <this.NextArrow />,
+			prevArrow: <this.PrevArrow />,
+
+			responsive: [
+				{
+					breakpoint: 766,
+					settings: {
+						slidesToShow: 2,
+						centerMode: true
+					}
+				}
+			]
+		};
+		return (
+			<React.Fragment>
+				<div className="icons-wrapper justify-content-around d-none d-md-flex">
+					{this.items.map((item) => {
+						return (
+							<div className="icon" key={item.name}>
+								<img src={item.img} alt={item.img} className="img lazy" />
+								<p className="text">{item.name}</p>
+							</div>
+						);
+					})}
+				</div>
+				<div className="icons-wrapper-slick d-md-none">
+					<Slider {...settings}>
+						{this.items.map((item) => {
+							return (
+								<div className="icon" key={item.name}>
+									<img src={item.img} alt={item.img} className="img lazy m-auto" />
+									<p className="text text-center">{item.name}</p>
+								</div>
+							);
+						})}
+					</Slider>
+				</div>
+			</React.Fragment>
+		);
+	}
+}
 
 export default Inclusives;
--- src/Containers/Building/index.jsx
+++ src/Containers/Building/index.jsx
@@ -1,157 +1,145 @@
-import React, { Component, Fragment } from "react";
-import LazyLoad from "react-lazyload";
+import React, { Component, Fragment } from 'react';
+import LazyLoad from 'react-lazyload';
 
-import Cover from "../../Components/Cover";
-import Rooms from "../../Components/Rooms";
-import Inclusive from "../../Components/Inclusive";
-import GoogleStaticMap from "../../Components/GoogleStaticMap";
-import OtherProperties from "../../Components/OtherProperties";
-import Footer from "../../Components/Footer";
-import Header from "../../Components/Header";
+import Cover from '../../Components/Cover';
+import Rooms from '../../Components/Rooms';
+import Inclusive from '../../Components/Inclusive';
+import GoogleStaticMap from '../../Components/GoogleStaticMap';
+import OtherProperties from '../../Components/OtherProperties';
+import Footer from '../../Components/Footer';
+import Header from '../../Components/Header';
 
-import LandingCover from "./LandingCover";
-import Yellow2nut from "../../Components/Yellow2nut";
+import LandingCover from './LandingCover';
+import Yellow2nut from '../../Components/Yellow2nut';
 
 // css starts
-import "./style.css";
-import ErrorBoundary from "../ErrorBoundary";
-import GalleryNew from "../../Components/GalleryNew";
+import './style.css';
+import ErrorBoundary from '../ErrorBoundary';
+import GalleryNew from '../../Components/GalleryNew';
 
 class Building extends Component {
-  state = {
-    buildingData: {},
-    nearbyProperties: {},
-    footer: {
-      logoLink: "https://grexter.in",
-      phone: "8880968000",
-      instagram: "https://www.instagram.com/grexter_living/",
-      facebook: "https://www.facebook.com/grexterhousing/",
-      linkedin: "https://www.linkedin.com/company/grexter"
-    },
-    bookVisitClicked: false,
-    selectOptionsar: [],
-    selectOption: "",
-    loader: true
-  };
+	state = {
+		buildingData: {},
+		nearbyProperties: {},
+		footer: {
+			logoLink: 'https://grexter.in',
+			phone: '8880968000',
+			instagram: 'https://www.instagram.com/grexter_living/',
+			facebook: 'https://www.facebook.com/grexterhousing/',
+			linkedin: 'https://www.linkedin.com/company/grexter'
+		},
+		bookVisitClicked: false,
+		selectOptionsar: [],
+		selectOption: '',
+		loader: true,
+		id: 42
+	};
 
-  componentDidMount() {
-    let params = window.location.search;
-    const id = params.split("=")[1] || 25;
-    this.getBuildingData(id);
-  }
+	componentDidMount() {
+		let params = window.location.search;
+		const id = params.split('=')[1] || 25;
+		this.getBuildingData(id);
+	}
 
-  getBuildingData = async id => {
-    this.setState({
-      loader: true
-    });
+	getBuildingData = async (id) => {
+		this.setState({
+			loader: true
+		});
 
-    try {
-      const res = await fetch(
-        `https://backend.grexter.in/buildings/${id}?include=location,amenities,landmarks,area,subarea`
-      );
-      const buildingData = await res.json();
+		try {
+			const res = await fetch(
+				`https://backend.grexter.in/buildings/${id}?include=location,amenities,landmarks,area,subarea`
+			);
+			const buildingData = await res.json();
 
-      const res1 = await fetch(
-        `https://backend.grexter.in/nearby-buildings?include=location,landmarks,area,layouts,layout_prices,building_images&lon=${buildingData.location.longitude}&lat=${buildingData.location.latitude}`
-      );
+			const res1 = await fetch(
+				`https://backend.grexter.in/nearby-buildings?include=location,landmarks,area,layouts,layout_prices,building_images&lon=${buildingData
+					.location.longitude}&lat=${buildingData.location.latitude}`
+			);
 
-      let nearbyProperties = await res1.json();
+			let nearbyProperties = await res1.json();
 
-      const selectOptionsar = [];
+			const selectOptionsar = [];
 
-      nearbyProperties.forEach(data => {
-        selectOptionsar.push({
-          name: data.name,
-          id: data.id
-        });
-      });
+			nearbyProperties.forEach((data) => {
+				selectOptionsar.push({
+					name: data.name,
+					id: data.id
+				});
+			});
 
-      nearbyProperties = nearbyProperties.slice(1, 4);
-      this.setState({
-        buildingData,
-        nearbyProperties,
-        selectOptionsar,
-        loader: false
-      });
-    } catch (err) {
-      console.log(err.status);
-    }
-  };
+			nearbyProperties = nearbyProperties.slice(1, 4);
+			this.setState({
+				buildingData,
+				nearbyProperties,
+				selectOptionsar,
+				loader: false
+			});
+		} catch (err) {
+			console.log(err.status);
+		}
+	};
 
-  bookVisitClickHandler = async () => {
-    await setTimeout(() => {
-      this.setState({
-        bookVisitClicked: true
-      });
-    }, 800);
-    setTimeout(() => {
-      this.setState({ bookVisitClicked: false });
-    }, 810);
-  };
+	bookVisitClickHandler = async () => {
+		await setTimeout(() => {
+			this.setState({
+				bookVisitClicked: true
+			});
+		}, 800);
+		setTimeout(() => {
+			this.setState({ bookVisitClicked: false });
+		}, 810);
+	};
 
-  render() {
-    const {
-      nearbyProperties,
-      bookVisitClicked,
-      selectOptionsar,
-      loader
-    } = this.state;
-    const {
-      address,
-      images,
-      name,
-      description,
-      layouts = []
-    } = this.state.buildingData;
+	render() {
+		const { nearbyProperties, bookVisitClicked, selectOptionsar, loader } = this.state;
+		const { address, images, name, description, layouts = [] } = this.state.buildingData;
 
-    return loader ? (
-      <div className="loader" />
-    ) : (
-      <Fragment>
-        <Header />
-        <Cover images={images}>
-          <ErrorBoundary>
-            <LandingCover
-              name={name}
-              desc={description}
-              bookVisitClicked={bookVisitClicked}
-              selectOptionsar={selectOptionsar}
-            />
-          </ErrorBoundary>
-        </Cover>
-        <Yellow2nut text="Rooms" />
-        <LazyLoad>
-          <Rooms
-            layouts={layouts}
-            bookVisitClickHandler={this.bookVisitClickHandler}
-          />
-        </LazyLoad>
-        <LazyLoad>
-          <Yellow2nut text="Inclusive" />
-        </LazyLoad>
+		return loader ? (
+			<div className="loader" />
+		) : (
+			<Fragment>
+				<Header />
+				<Cover images={images}>
+					<ErrorBoundary>
+						<LandingCover
+							name={name}
+							desc={description}
+							bookVisitClicked={bookVisitClicked}
+							selectOptionsar={selectOptionsar}
+						/>
+					</ErrorBoundary>
+				</Cover>
+				<Yellow2nut text="Rooms" />
+				<LazyLoad>
+					<Rooms layouts={layouts} bookVisitClickHandler={this.bookVisitClickHandler} />
+				</LazyLoad>
+				<LazyLoad>
+					<Yellow2nut text="Inclusive" />
+				</LazyLoad>
 
-        <Inclusive />
+				<Inclusive />
 
-        {images.length ? (
-          <Fragment>
-            <Yellow2nut text="Gallery" />
-            <LazyLoad>
-              <GalleryNew images={images} />
-            </LazyLoad>
-          </Fragment>
-        ) : null}
+				{images.length ? (
+					<Fragment>
+						<Yellow2nut text="Gallery" />
+						<LazyLoad>
+							<GalleryNew images={images} />
+						</LazyLoad>
+					</Fragment>
+				) : null}
 
-        <Yellow2nut text="Address and Maps  " />
-        <GoogleStaticMap address={address} name={name} />
-        <Yellow2nut text="Other Properties" />
-        <LazyLoad>
-          <OtherProperties nearby={nearbyProperties} />
-        </LazyLoad>
+				<Yellow2nut text="Address and Maps  " />
+				<GoogleStaticMap address={address} name={name} />
+				<Yellow2nut text="Other Properties" />
+				<LazyLoad>
+					<OtherProperties nearby={nearbyProperties} />
+				</LazyLoad>
 
-        <Footer {...this.state.footer} />
-      </Fragment>
-    );
-  }
+				<Footer {...this.state.footer} />
+			</Fragment>
+		);
+	}
 }
 
 export default Building;
